df0f13479241501f5902b2e6be76bf35176c5ec3
Adam Lancaster <lancaster28@hotmail.com>
2024-02-21 11:31:55 -0700

    remove comments|

--- lib/jxon_slim_original.ex
+++ lib/jxon_slim_original.ex
@@ -206,7 +206,6 @@ defmodule JxonSlimOriginal do
     {:error, :invalid_json_character, current_index}
   end
 
-  # @compile {:inline, parse_object: 6}
   defp parse_object(<<rest::binary>>, original, handler, current_index, acc, depth_stack) do
     case handler.start_of_object(original, current_index - 1, acc) do
       {:error, _, _} = error ->
@@ -218,8 +217,6 @@ defmodule JxonSlimOriginal do
         key_value(rest, original, handler, end_index, acc, depth_stack)
     end
   end
-
-  # @compile {:inline, key_value: 6}
 
   defp key_value(<<@close_object, rest::bits>>, original, handler, index, acc, depth_stack) do
     close_object(rest, original, handler, index, acc, depth_stack)
@@ -278,8 +275,6 @@ defmodule JxonSlimOriginal do
     end
   end
 
-  # @compile {:inline, parse_object_key: 5}
-
   defp parse_object_key(
          <<@quotation_mark, rest::bits>> = j,
          original,
@@ -315,8 +310,6 @@ defmodule JxonSlimOriginal do
     {:error, :invalid_object_key, index}
   end
 
-  # @compile {:inline, parse_array: 6}
-
   defp parse_array(<<array_contents::binary>>, original, handler, current_index, acc, depth_stack) do
     # current index points to head of array_contents, we want the char before ie the '['
     case handler.start_of_array(original, current_index - 1, acc) do
@@ -340,8 +333,6 @@ defmodule JxonSlimOriginal do
         end
     end
   end
-
-  # @compile {:inline, parse_values: 6}
 
   defp parse_values(<<@close_array, _rest::bits>>, original, handler, index, acc, [
          {@array, array_depth} | rest_depth
@@ -404,7 +395,6 @@ defmodule JxonSlimOriginal do
     end
   end
 
-  # @compile {:inline, parse_value: 6}
   defp parse_value(
          <<@open_array, rest::bits>>,
          original,
@@ -745,7 +735,7 @@ defmodule JxonSlimOriginal do
 
   defp skip_whitespace(<<_remaining::binary>>, index), do: index
 
-  # What happens if the backslash is escaped? Should be an error... no?
+  # TODO: implement good string escaping here.
   defp parse_string(<<@backslash, @backslash, @quotation_mark, _::bits>>, end_character_index) do
     {:error, :unescaped_quotation_mark, end_character_index + 2}
   end
